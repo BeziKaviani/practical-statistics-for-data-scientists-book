d0f304276c9bc83e77693fa4c351f00debc9344a
BEZI <behzadkaviani2004@gmail.com>
2025-12-06 01:29:39 +0330

    add location-estimates.r

---
+++ 1-Exploratory data analysis/3-Estimates of location/location-estimates.r
@@ -0,0 +1,13 @@
+# normal mean
+mean(states[['Population']])
+
+# 10 percent of the value are omitted (from the top and down)
+mean(states[['Population']], trim = 0.1)
+
+# median
+median(states[['Population']])
+
+
+
+
+
